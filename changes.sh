621c1cfa214ebe8535b4b36a27d54a5fe6c8a7e7
wAsabi-007 <check_again@protonmail.com>
2019-12-24 13:59:04 -0800

    killing bluetooth

--- changes.sh
+++ changes.sh
@@ -6,7 +6,12 @@ sudo apt-get purge chromium-browser
 sudo apt-get purge --auto-remove chromium-browser
 sudo apt-get purge cups-daemon
 sudo apt-get purge --auto-remove cups-daemon
+sudo apt-get remove xfce4
+sudo apt-get remove --auto-remove xfce4
+sudo apt-get purge xfce4
+sudo apt-get purge --auto-remove xfce4
 sudo apt install i3 xss-lock lightdm
 firefox -profile ~/.mozilla/firefox/default.Default
 wget https://raw.githubusercontent.com/pyllyukko/user.js/master/user.js ~
 firefox -profile ~/.mozilla/firefox/default.Default
+rfkill block bluetooth
