c07f5e6b66ccb3943d1625fb9b11312bf68a5f28
test1 <test1@test1.test1>
2020-10-11 21:36:02 -0700

    adding .bashrc command

--- changes.sh
+++ changes.sh
@@ -148,6 +148,7 @@ sudo cp ~/dotfiles/confs/logrotate.conf /etc/
 sudo cp ~/dotfiles/confs/grub /etc/default/grub
 sudo cp ~/dotfiles/confs/logrotate /etc/cron.hourly/
 sudo cp ~/dotfiles/confs/NetworkManager-inter.conf /var/lib/NetworkManager/NetworkManager-inter.conf
+sudo cp ~/dotfiles/confs/.bashrc ~/
 
 # enable services
 sudo systemctl enable iptables.service
